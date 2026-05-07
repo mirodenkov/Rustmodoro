3d40a31b17db267224d6f53b8641fd5adfe71ba4
somerandomduh <miroslavdenkov10@proton.me>
2026-05-07 11:03:11 +0200

    Implemented basic timer logic

--- src/timer.rs
+++ src/timer.rs
@@ -1,4 +1,5 @@
 /*Enum with timer states */
+#[derive(Debug, Clone, PartialEq)]
 pub enum TimerState {
     Running,
     Paused,
@@ -7,19 +8,37 @@ pub enum TimerState {
 }
 
 pub struct Timer {
-    pub remaining_secs: u64,
-    pub starting_secs: u64,
-    pub state: TimerState,
+    remaining_secs: u64,
+    starting_secs: u64,
+    state: TimerState,
 }
 
-trait TimerControl {
+pub trait TimerControl {
     fn tick(&mut self); //&mut self means that it reads and writes the struct
     fn get_remaining(&self) -> u64; // returns how many seconds are remaining
-    fn reset(&mut self) -> bool;
-    fn pause(&mut self) -> bool;
-    fn start(&mut self) -> bool;
+    fn reset(&mut self);
+    fn pause(&mut self) -> Result<(), String>;
+    fn start(&mut self) -> Result<(), String>;
 }
 
+//Constructor
+impl Timer {
+    pub fn new(minutes: u64) -> Self {
+        let secs = minutes * 60;
+        Self {
+            remaining_secs: secs,
+            starting_secs: secs,
+            state: TimerState::Reset,
+        }
+    }
+
+    pub fn state(&self) -> &TimerState {
+        &self.state
+    }
+    pub fn remaining(&self) -> u64 {
+        self.remaining_secs
+    }
+}
 
 impl TimerControl for Timer {
     fn tick(&mut self) {
@@ -32,19 +51,25 @@ impl TimerControl for Timer {
     }
     fn get_remaining(&self) -> u64 {
         self.remaining_secs
-        //note to self: tis the same as return self.remaining_secs;
     }
-    fn reset(&mut self) -> bool {
-        self.remaining_secs = 0; //reseting to zero
+    fn reset(&mut self) {
+        self.remaining_secs = self.starting_secs; //reseting to starting_secs
         self.state = TimerState::Reset;
-        true
     }
-    fn pause(&mut self) -> bool {
+    fn pause(&mut self) -> Result<(), String> {
+        if self.state != TimerState::Running {
+            return Err("Can't pause a timer that isn't running".to_string());
+        }
         self.state = TimerState::Paused;
-        true
+        Ok(())
     }
-    fn start(&mut self) -> bool {
+    fn start(&mut self) -> Result<(), String> {
+        if self.state == TimerState::Finished && self.remaining_secs == 0 {
+            return Err("Can't start a finished timer".to_string());
+        } else if self.state == TimerState::Running {
+            return Err("Timer is already running".to_string());
+        }
         self.state = TimerState::Running;
-        true
+        Ok(())
     }
 }
