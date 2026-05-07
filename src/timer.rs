d1d5bae800045f0a5b605c6b0eacf5ca1b75a1ce
somerandomduh <miroslavdenkov10@proton.me>
2026-05-07 10:23:19 +0200

    Cleaned out traits and optimized timer.rs file

--- src/timer.rs
+++ src/timer.rs
@@ -12,37 +12,16 @@ pub struct Timer {
     pub state: TimerState,
 }
 
-//traits are like contracts, they announce what the timer will do and then we need to implement it
-trait Countdown {
+trait TimerControl {
     fn tick(&mut self); //&mut self means that it reads and writes the struct
     fn get_remaining(&self) -> u64; // returns how many seconds are remaining
-}
-
-trait Reset {
     fn reset(&mut self) -> bool;
-}
-
-trait Pause {
     fn pause(&mut self) -> bool;
-}
-
-trait Start {
     fn start(&mut self) -> bool;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////
-/// Implementations
-///
-///
-///
-///
-///
 
-//pub fn get_state(&self) -> &TimerState {
-  //  &self.state
-//}
-
-impl Countdown for Timer {
+impl TimerControl for Timer {
     fn tick(&mut self) {
         if matches!(self.state, TimerState::Running) {
             self.remaining_secs = self.remaining_secs.saturating_sub(1);
@@ -55,24 +34,15 @@ impl Countdown for Timer {
         self.remaining_secs
         //note to self: tis the same as return self.remaining_secs;
     }
-}
-
-impl Reset for Timer {
     fn reset(&mut self) -> bool {
         self.remaining_secs = 0; //reseting to zero
         self.state = TimerState::Reset;
         true
     }
-}
-
-impl Pause for Timer {
     fn pause(&mut self) -> bool {
         self.state = TimerState::Paused;
         true
     }
-}
-
-impl Start for Timer {
     fn start(&mut self) -> bool {
         self.state = TimerState::Running;
         true
